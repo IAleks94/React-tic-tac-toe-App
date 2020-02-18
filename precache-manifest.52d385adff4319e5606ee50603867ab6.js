66c29239ea7027752211b9033d1bfe7e27d6df9f
IAleks94 <a12344@mail.ru>
2020-02-18 12:27:07 +0300

    Updates

---
+++ precache-manifest.52d385adff4319e5606ee50603867ab6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e35fdacd6de45d8fa14076c0202e333",
+    "url": "/React-tic-tac-toe-App/index.html"
+  },
+  {
+    "revision": "0c9fc464e3cfc967491f",
+    "url": "/React-tic-tac-toe-App/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "0be154a3e0c083d1f41f",
+    "url": "/React-tic-tac-toe-App/static/js/2.718ed629.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-tic-tac-toe-App/static/js/2.718ed629.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c9fc464e3cfc967491f",
+    "url": "/React-tic-tac-toe-App/static/js/main.4e63d203.chunk.js"
+  },
+  {
+    "revision": "a68d38d64b0f3c50f065",
+    "url": "/React-tic-tac-toe-App/static/js/runtime-main.3bc14501.js"
+  }
+]);
